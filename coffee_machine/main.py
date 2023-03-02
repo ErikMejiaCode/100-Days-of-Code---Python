cdda48a3e4eead18e8774bccd3e4862b85e6cdec
Erik Mejia <erikdojo352@gmail.com>
2023-03-01 16:05:32 -0800

    Print line added for shutting down.

--- coffee_machine/main.py
+++ coffee_machine/main.py
@@ -75,6 +75,7 @@ is_on = True
 while is_on:
     choice = input("What would you like? (espresso/latte/cappuccino): ")
     if choice == 'off':
+        print("Shutting down.")
         is_on = False
     elif choice == 'report':
         print(f"Water: {resources['water']}ml")
