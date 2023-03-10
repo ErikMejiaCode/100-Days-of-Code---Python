b21484c43f3480159098607b464e18237924f661
Erik Mejia <erikdojo352@gmail.com>
2023-03-10 17:10:08 -0500

    Ball class and collision

--- pong-game/main.py
+++ pong-game/main.py
@@ -1,13 +1,17 @@
 from turtle import Screen
 from player import Player
+from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor('black')
 screen.title("Pong")
+screen.tracer(0)
 
 player1 = Player((350, 0))
 player2 = Player((-350, 0))
+ball = Ball()
 
 
 screen.listen()
@@ -15,6 +19,21 @@ screen.onkey(fun=player1.up, key='Up')
 screen.onkey(fun=player1.down, key="Down")
 screen.onkey(fun=player2.up, key='w')
 screen.onkey(fun=player2.down, key="s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    # Detect collision with top wall or bottom wall
+    if ball.ycor() > 280 or ball.ycor() < -277:
+        # Needs to bounce
+        ball.bounce_y()
+    
+    # Detect collision with the players
+    if ball.distance(player1) < 50 and ball.xcor() > 320 or ball.distance(player2) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
 
 
 screen.exitonclick()
