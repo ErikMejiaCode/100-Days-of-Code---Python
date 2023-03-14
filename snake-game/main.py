5b3def7e0f280b6ac7f0a864e2106e0f2a5adadb
Erik Mejia <erikdojo352@gmail.com>
2023-03-14 14:41:40 -0400

    High score added

--- snake-game/main.py
+++ snake-game/main.py
@@ -36,15 +36,15 @@ while game_is_on:
 
     # Detect collision with wall
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
-        game_is_on = False
-        score_board.game_over()
+        score_board.reset()
+        snake.reset()
 
     # Detect collision with own tail
     # Using Slicing to loop through all segments EXCEPT the first one = head
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            score_board.game_over()
+            score_board.reset()
+            snake.reset()
 
 
 screen.exitonclick()
