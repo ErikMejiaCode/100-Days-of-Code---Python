9d3e1431b75be790a4708d239ef8e35a07250d88
Erik Mejia <erikdojo352@gmail.com>
2023-03-17 13:55:59 -0400

    list comprehension used in creating new lists

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -17,13 +17,14 @@ while len(guessed_states) < 50:
                                     prompt="What's another state's name? ").title()
 
     if answer_state == "Exit":
-        missing_states = []
-        for state in state_names_list:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in state_names_list if state not in guessed_states]
+        # for state in state_names_list:
+        #     if state not in guessed_states:
+        #         missing_states.append(state)
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv('states_to_learn')
         break
+
     # If answer state is one of the states in all 50 states
     if answer_state in state_names_list and answer_state not in guessed_states:
         guessed_states.append(answer_state)
